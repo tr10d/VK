1022514b2106a8bf2d5e58440a25267a2a2fb5a0
s_ser <s_ser@list.ru>
2020-12-26 17:34:45 +0300

    task 2

--- VK/UIClasses/LoginViewController.swift
+++ VK/UIClasses/LoginViewController.swift
@@ -9,22 +9,20 @@ import UIKit
 
 class LoginViewController: UIViewController {
 
+    // MARK: - IBOutlet
+
     @IBOutlet weak var login: UITextField!
     @IBOutlet weak var password: UITextField!
     @IBOutlet weak var scrollView: UIScrollView!
     
+    // MARK: - UIViewController
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(notification:)), name: UIResponder.keyboardWillShowNotification, object: nil)
         NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(notification:)), name: UIResponder.keyboardWillHideNotification, object: nil)
-
     }
 
-    @IBAction func enter(_ sender: UIButton) {
-        print("Hello")
-    }
-  
     @objc func keyboardWillShow(notification: Notification) {
         guard let kbSize = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? CGRect else { return }
         let insets = UIEdgeInsets(top: 0, left: 0, bottom: kbSize.size.height, right: 0)
@@ -35,6 +33,25 @@ class LoginViewController: UIViewController {
         let insets = UIEdgeInsets.zero
         scrollView.contentInset = insets
     }
+ 
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+//        print("prepare")
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+
+    // MARK: - IBAction
     
+    @IBAction func login(_ sender: UIButton) {
+        
+        // MARK: переход на другой экран (способ 1)
+//        performSegue(withIdentifier: "toMainTab", sender: sender)
+        
+        // MARK: переход на другой экран (способ 2)
+//        let destinationUIView = UIStoryboard(name: "Main", bundle: nil)
+//            .instantiateViewController(withIdentifier: "")
+//        present(destinationUIView, animated: true)
+    }
+ 
 }
 
