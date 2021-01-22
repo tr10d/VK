595ab081abf5b6804e791b79162ecee901253cca
s_ser <s_ser@list.ru>
2021-01-22 19:40:00 +0300

    Добавлена анимация аватарки пользователя/группы

--- VK/Views/GroupTableViewCell.swift
+++ VK/Views/GroupTableViewCell.swift
@@ -15,6 +15,14 @@ class GroupTableViewCell: UITableViewCell {
     @IBOutlet weak var groupImage: UIImageView!
     @IBOutlet weak var groupName: UILabel!
 
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        let tapGestureRecognizer = UITapGestureRecognizer(target: self,
+                                                    action: #selector(imageTapped(tapGestureRecognizer:)))
+        self.groupImage.isUserInteractionEnabled = true
+        self.groupImage.addGestureRecognizer(tapGestureRecognizer)
+    }
+
     override func prepareForReuse() {
         groupImage.image = nil
         groupName.text = nil
@@ -24,6 +32,17 @@ class GroupTableViewCell: UITableViewCell {
         guard let group = group else { return }
         groupImage.image = group.image.image
         groupName.text = group.name
+    }
+
+    @objc
+    func imageTapped(tapGestureRecognizer: UITapGestureRecognizer) {
+        let springAnimation = CASpringAnimation(keyPath: "transform.scale")
+        springAnimation.fromValue = 0.7
+        springAnimation.toValue = 1
+        springAnimation.mass = 0.3
+        springAnimation.damping = 1
+
+        tapGestureRecognizer.view?.layer.add(springAnimation, forKey: nil)
     }
 
 }
---
+++ VK/Views/Loading.swift
@@ -0,0 +1,68 @@
+//
+//  Loading.swift
+//  VK
+//
+//  Created by  Sergei on 22.01.2021.
+//
+
+import UIKit
+
+@IBDesignable
+class Loading: UIControl {
+
+    private var stackView: UIStackView!
+    private var circle1, circle2, circle3: UIImageView!
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupView​()
+        animateView​()
+    }
+
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setupView​()
+        animateView​()
+    }
+
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        stackView.frame = bounds
+    }
+
+    private func setupView​() {
+
+        let color = #colorLiteral(red: 0.501960814, green: 0.501960814, blue: 0.501960814, alpha: 1)
+        circle1 = UIImageView(image: UIImage(systemName: "circle.fill"))
+        circle1.tintColor = color
+        circle2 = UIImageView(image: UIImage(systemName: "circle.fill"))
+        circle2.tintColor = color
+        circle3 = UIImageView(image: UIImage(systemName: "circle.fill"))
+        circle3.tintColor = color
+
+        stackView = UIStackView(arrangedSubviews: [circle1, circle2, circle3])
+        stackView.axis = .horizontal
+        stackView.alignment = .fill
+        stackView.distribution = .equalSpacing
+
+        addSubview(stackView)
+
+    }
+
+    func animateView​() {
+        let alpha: CGFloat = 0.2
+        let duration: Double = 1
+        let options: UIView.AnimationOptions = [.repeat, .autoreverse]
+
+        UIView.animate(withDuration: duration, delay: 0, options: options) {
+            self.circle1.alpha = alpha
+        }
+        UIView.animate(withDuration: duration, delay: 0.4, options: options) {
+            self.circle2.alpha = alpha
+        }
+        UIView.animate(withDuration: duration, delay: 0.8, options: options) {
+            self.circle3.alpha = alpha
+        }
+
+    }
+}
